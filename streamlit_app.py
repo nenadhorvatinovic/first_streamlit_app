1f9f3438cea5672ca8f5c5c8d855d44860a7e55e
Nenad Horvatinović <118742413+nenadhorvatinovic@users.noreply.github.com>
2022-11-28 14:10:15 +0100

    Update streamlit_app.py
    added fruit_choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,10 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+
 # setiraj vrijednost variable za tablicu
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # prikazi tablicu
